4ef66305da247cc99dd85d15b7de29c4b8b01dc8
maiger <mage_ege@hotmail.com>
2017-02-15 17:56:34 +0200

    Continue work on AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+public class EnemyAI : MonoBehaviour {
+
+    public Transform target;
+
+    public float updateRate = 2f;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    // The calculated path
+    public Path path;
+
+    // The AI's speed per second
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    // The max distance from the AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
+
+    private int currentWaypoint = 0;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if(target == null)
+        {
+            Debug.LogError("Np player found!");
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        // write some more
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("We got a path. Did it have an error? " + p.error);
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
