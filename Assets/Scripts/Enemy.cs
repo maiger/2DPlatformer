cf2baaf68978cb9c720c61ea3cb8b375a0e8b7df
maiger <mage_ege@hotmail.com>
2017-02-21 17:21:19 +0200

    Add enemy gui
    Add a heath bar and health text above enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,19 +7,50 @@ public class Enemy : MonoBehaviour {
     [System.Serializable]
     public class EnemyStats
     {
-        public int Health = 100;
+        public int maxHealth = 100;
 
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+        }
+
+
+        public void Init()
+        {
+            curHealth = maxHealth;
+        }
     }
 
     public EnemyStats stats = new EnemyStats();
 
+    [Header("Optional: ")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    void Start()
+    {
+        stats.Init();
+
+        if(statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+        }
+    }
+
     public void DamageEnemy(int damage)
     {
-        stats.Health -= damage;
+        stats.curHealth -= damage;
 
-        if (stats.Health <= 0)
+        if (stats.curHealth <= 0)
         {
             GameMaster.KillEnemy(this);
+        }
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
         }
     }
 }
