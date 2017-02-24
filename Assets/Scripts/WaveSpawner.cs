abfce351bfb19edfa77990f199c223e6ad2e1a87
maiger <mage_ege@hotmail.com>
2017-02-24 16:55:44 +0200

    Improve wave spawner

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -17,18 +17,39 @@ public class WaveSpawner : MonoBehaviour {
     public Wave[] waves;
     private int nextWave = 0;
 
+    public Transform[] spawnPoints;
+
     public float timeBetweenWaves = 5f;
-    public float waveCountdown;
+    private float waveCountdown;
+
+    private float searchCountdown = 1f;
 
     private SpawnState state = SpawnState.COUNTING;
 
     private void Start()
     {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points referenced.");
+        }
+
         waveCountdown = timeBetweenWaves;
     }
 
     private void Update()
     {
+        if(state == SpawnState.WAITING)
+        {
+            if (EnemyIsAlive() == false)
+            {
+                WaveCompleted();
+                return;
+            } else
+            {
+                return;
+            }
+        }
+
         if(waveCountdown <= 0)
         {
             if(state != SpawnState.SPAWNING)
@@ -41,8 +62,42 @@ public class WaveSpawner : MonoBehaviour {
         }
     }
 
+    void WaveCompleted()
+    {
+        Debug.Log("Wave completed!");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("ALL WAVES COMPLETE! Looping..");
+        } else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if(searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
     IEnumerator SpawnWave(Wave _wave)
     {
+        Debug.Log("Spawning wave: " + _wave.name);
         state = SpawnState.SPAWNING;
 
         for (int i = 0; i < _wave.count; i++)
@@ -58,7 +113,9 @@ public class WaveSpawner : MonoBehaviour {
 
     void SpawnEnemy(Transform _enemy)
     {
-        // Spawn enemy
         Debug.Log("SPawning an enemy: " + _enemy.name);
+
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
     }
 }
